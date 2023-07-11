a6391573db0ad9ebe7f8204ffa49c5cfd9fe7a56
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-11 11:35:15 -0300

    feat: Soluciona desafio #3 Tabuada

--- exercises/multiplication_table.rb
+++ exercises/multiplication_table.rb
@@ -1,7 +1,7 @@
 class MultiplicationTable
   def self.calculate(number)
     #your code here \/ \/ \/
-
+    (1..10).map { |factor| "#{number}x#{factor} = #{number * factor}" }.join(', ')
     #your code here /\ /\ /\
   end
 end
