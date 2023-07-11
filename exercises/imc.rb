e0a3c6f4d07210c7a861288f4dbe16f28ec69a11
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-11 10:09:32 -0300

    feat: Soluciona desafio #2 IMC

--- exercises/imc.rb
+++ exercises/imc.rb
@@ -1,7 +1,14 @@
 class Imc
   def self.calculate(height,weight)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    # your code here \/ \/ \/
+    height = height.to_f / 100
+    imc = (weight.to_f / (height * height)).round 2
+    return "#{imc} - Abaixo do peso" if imc < 18.5
+    return "#{imc} - peso normal" if (18.5..24.9).include?(imc)
+    return "#{imc} - Sobrepeso" if (25..29.9).include?(imc)
+    return "#{imc} - Obesidade grau 1" if (30..34.9).include?(imc)
+    return "#{imc} - Obesidade grau 2" if (35..39.9).include?(imc)
+    return "#{imc} - Obesidade grau 3" if imc > 40
+    # your code here /\ /\ /\
   end
 end
