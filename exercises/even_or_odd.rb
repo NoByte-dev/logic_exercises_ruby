00a1a500fd91643ae313ef1171205becf7b6149d
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-12 09:32:52 -0300

    feat: Soluciona desafio #4 Par ou Impar

--- exercises/even_or_odd.rb
+++ exercises/even_or_odd.rb
@@ -1,7 +1,8 @@
 class EvenOrOdd
   def self.calculate(number)
     #your code here \/ \/ \/
-
+    result = number.integer? ? number.to_i : number.to_s.split('.')[1].to_i
+    result.even? ? "#{number} is even" : "#{number} is odd"
     #your code here /\ /\ /\
   end
 end
