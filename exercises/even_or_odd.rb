39b938e204c558f28f49a76374b89405b1c9268b
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-12 10:02:45 -0300

    refactor: Melhora solução

--- exercises/even_or_odd.rb
+++ exercises/even_or_odd.rb
@@ -1,7 +1,7 @@
 class EvenOrOdd
   def self.calculate(number)
     #your code here \/ \/ \/
-    result = number.integer? ? number.to_i : number.to_s.split('.')[1].to_i
+    result = number.to_s.split('.').last.to_i
     result.even? ? "#{number} is even" : "#{number} is odd"
     #your code here /\ /\ /\
   end
