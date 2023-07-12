e29ff82aae22af798e9157c534dd596127124e68
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-12 10:30:36 -0300

    feat: Soluciona desafio #5 Juros Simples

--- exercises/simple_interest.rb
+++ exercises/simple_interest.rb
@@ -1,7 +1,8 @@
 class SimpleInterest
   def self.calculate(initial_amount, fee, months)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    # your code here \/ \/ \/
+    result = (initial_amount * ((months * fee) / 100.0)) + initial_amount
+    "R$ #{result.round(2)}"
+    # your code here /\ /\ /\
   end
 end
