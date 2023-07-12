3f8bb0a2bb5873fcc88f85ef5aa0c0863d907a59
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-12 12:20:04 -0300

    feat: Soluciona desafio #7 Juros Compostos

--- exercises/compound_interest.rb
+++ exercises/compound_interest.rb
@@ -1,7 +1,8 @@
 class CompoundInterest
   def self.calculate(initial_amount, fee, months)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    # your code here \/ \/ \/
+    result = initial_amount * (1 + fee / 100.0) ** months
+    "R$ #{result.round(2)}"
+    # your code here /\ /\ /\
   end
 end
