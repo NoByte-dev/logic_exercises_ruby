40a75229a7e7f7eb2da0ff6723f445525bffa720
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-13 09:10:17 -0300

    feat: Soluciona desafio #8 Apertos de mão

--- exercises/handshake.rb
+++ exercises/handshake.rb
@@ -1,7 +1,7 @@
 class Handshake
   def self.calculate(number)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    # your code here \/ \/ \/
+    number * (number - 1) / 2
+    # your code here /\ /\ /\
   end
 end
