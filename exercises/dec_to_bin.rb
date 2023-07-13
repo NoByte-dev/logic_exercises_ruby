29f89433b5bed80d42c2a9e0721287592a346422
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-13 09:53:43 -0300

    refactor: #10 Decimal > Binário

--- exercises/dec_to_bin.rb
+++ exercises/dec_to_bin.rb
@@ -1,12 +1,7 @@
 class DecToBin
   def self.convert(number)
     # your code here \/ \/ \/
-    binary = ''
-    while number.positive?
-      binary = (number % 2).to_s + binary
-      number /= 2
-    end
-    binary.to_i
+    number.to_s(2).to_i
     # your code here /\ /\ /\
   end
 end
