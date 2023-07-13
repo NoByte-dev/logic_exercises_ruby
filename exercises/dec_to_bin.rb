693793403dd7d2cc353029fd636f19424c277c04
NoByte-dev <nobyte_dev@hotmail.com>
2023-07-13 09:24:39 -0300

    feat: Soluciona desafio #10 Decimal > Binário

--- exercises/dec_to_bin.rb
+++ exercises/dec_to_bin.rb
@@ -1,7 +1,12 @@
 class DecToBin
   def self.convert(number)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    # your code here \/ \/ \/
+    binary = ''
+    while number.positive?
+      binary = (number % 2).to_s + binary
+      number /= 2
+    end
+    binary.to_i
+    # your code here /\ /\ /\
   end
 end
